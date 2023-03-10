fe7561d30982741b7a45afd4de07a1ba4bb683ee
Sergei Baranovski <sergei@deriv.com>
2023-03-10 15:25:21 +0300

    feat: try inline template

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  //   styleUrls: ['./server.component.css'],
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent {}
