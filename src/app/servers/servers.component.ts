56e87014d0b31a33b7dfbe0418ce4c03d89bb25f
Sergei Baranovski <sergei@deriv.com>
2023-03-14 16:10:22 +0300

    feat: $event example

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,6 +14,7 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -23,5 +24,9 @@ export class ServersComponent {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
