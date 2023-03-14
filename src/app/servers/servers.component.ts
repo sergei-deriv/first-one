2ab33f80667c1d1c81dd74aa66c595adbb4df27b
Sergei Baranovski <sergei@deriv.com>
2023-03-14 15:29:48 +0300

    feat: property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,11 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-servers',
   // selector: '[app-servers]',
   // selector: '.app-servers',
-  // templateUrl: './servers.component.html',
-  template: `<app-server></app-server>
-    <app-server></app-server>
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  // template: `<app-server></app-server>
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+}
