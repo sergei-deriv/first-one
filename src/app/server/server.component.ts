eaa56642cb4e6fb9e4aa88d99bccdb2831ce97aa
Sergei Baranovski <sergei@deriv.com>
2023-03-14 15:19:24 +0300

    feat: play with string interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css'],
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+}
